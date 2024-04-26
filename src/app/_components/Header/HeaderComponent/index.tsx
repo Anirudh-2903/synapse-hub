93b6a8155b7974ce912eae8fa14f55018465138d
Anirudh <anirudh.das2903@outlook.in>
2024-04-26 22:17:40 +0530

    initial website created and hero added to homepage
    Sign-Up , Sign-In & Recover Password pages added
    
    Header and Footer added along with Nav Bar

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+"use client";
 
-const HeaderComponent = () => {
+import React from 'react';
+import { Header } from '../../../../payload/payload-types';
+import { Gutter } from '../../Gutter';
+import Link from 'next/link';
+
+import classes from './index.module.scss';
+import Image from 'next/image';
+import { HeaderNav } from '../Nav';
+import MobileNav from '../MobileNav';
+import { noHeaderFooterUrls } from '../../../constants';
+import { usePathname } from 'next/navigation';
+
+const HeaderComponent = ({header}: {header: Header}) => {
+
+  const pathname = usePathname();
+
   return (
-    <div>
-      index
-    </div>
+    <nav className={[classes.header , noHeaderFooterUrls.includes(pathname) && classes.hide]
+    .filter(Boolean)
+    .join(' ')}>
+      <Gutter className={classes.wrap}>
+        <Link href='/' >
+            <Image src="/logo-black.svg" alt='logo' width={200} height={50}/>
+        </Link>
+        <HeaderNav header={header} />
+        {/* <MobileNav header={header} /> */}
+      </Gutter>
+    </nav>
   )
 }
 
