c04b90e64b92fd113f066f3244f775ae1879e756
Anirudh <anirudh.das2903@outlook.in>
2024-04-30 00:17:48 +0530

    added own mongodb database for storing data and started working on filters on products page

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -1,11 +1,38 @@
+"use client";
+
 import React from 'react'
+import { useFilter } from '../../../_providers/Filter';
+import { Category } from '../../../../payload/payload-types';
 import classes from './index.module.scss';
+import { Checkbox } from '../../../_components/Checkbox';
+import { HR } from '../../../_components/HR';
 
 
-const Filters = () => {
+const Filters = ({categories}: {categories: Category[]}) => {
+
+  const { categoryFilters , sort , setSort ,  setCategoryFilters } = useFilter();
+  const handleCategories = (categoryId: string) => {
+  }
   return (
-    <div>
+    <div className={classes.filters}>
+        <div>
+         <h6 className={classes.title}>Product Categories</h6>
+         <div className={classes.categories}>
+            {categories.map((category) => {
+                const isSelected = categoryFilters.includes(category.id);
 
+                return(
+                    <Checkbox
+                        key={category.id}
+                        label={category.title}
+                        value={category.id}
+                        isSelected={isSelected}
+                        onClickHandler={handleCategories}  />
+                )
+            })}
+         </div>
+         <HR />
+        </div>
     </div>
   )
 }
